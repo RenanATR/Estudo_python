c72570aff520c75e021f5c2adb8b30b4a0c0b118
Renan Rodrigues <127256055+RenanATR@users.noreply.github.com>
2023-08-24 20:54:18 -0300

    Add files via upload

---
+++ exe.74-sorteio_numero_maior_menos_valor-random.py
@@ -0,0 +1,9 @@
+from random import randint
+
+numeros = (randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5))
+print('Os valores sorteados foram: ', end='')
+
+for n in numeros:
+        print(f'{n} ',end='')        
+print(f"\nO maior valor sorteado foi {max(numeros)}")
+print(f"O maior valor sorteado foi {min(numeros)}")
